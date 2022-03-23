17c25a91960c5fdac00c7a880e63e5ac1b4997d2
Lian <biakhong@gmail.com>
2022-03-23 18:36:27 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+}
+var bestCustomer; 
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(name){
+    bestCustomer = name;
+}
+const leastFavoriteCustomer = "you";
+
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer = name;
+}
